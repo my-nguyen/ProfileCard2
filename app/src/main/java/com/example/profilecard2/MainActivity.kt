70a16246fbc9b1ad4fe066a493486f7bbc5dc688
My Nguyen <mynguyen@MacBook-Pro-i9-64G-1T.local>
2025-09-15 17:24:21 -0700

    b2: Creating profile icon for card

--- app/src/main/java/com/example/profilecard2/MainActivity.kt
+++ app/src/main/java/com/example/profilecard2/MainActivity.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +48,17 @@ fun MainScreen() {
 @Composable
 fun ProfileCard() {
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(align = Alignment.Top),
         elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
     ) {
-        Row(modifier = Modifier.wrapContentSize()) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
             ProfilePicture()
             ProfileContent()
         }
@@ -49,12 +67,24 @@ fun ProfileCard() {
 
 @Composable
 fun ProfilePicture() {
-
+    Card(
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = Color.Green),
+        modifier = Modifier.padding(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile),
+            modifier = Modifier.size(72.dp),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Profile image",
+        )
+    }
 }
 
 @Composable
 fun ProfileContent() {
-
+    Text("Ken Wanatabe")
 }
 
 @Preview(showBackground = true)
