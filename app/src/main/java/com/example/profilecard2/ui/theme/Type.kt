6aadeeedf8d889d3e15e197993dbff28aee04425
My Nguyen <mynguyen@MacBook-Pro-i9-64G-1T.local>
2025-09-15 19:01:55 -0700

    b7: Customizing font type in theme

--- app/src/main/java/com/example/profilecard2/ui/theme/Type.kt
+++ app/src/main/java/com/example/profilecard2/ui/theme/Type.kt
@@ -14,7 +14,12 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    headlineSmall = TextStyle(
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        letterSpacing = 1.sp
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
